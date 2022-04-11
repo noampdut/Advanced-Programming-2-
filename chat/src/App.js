a470fb165396bb0701e03761c874589095b53ca6
Noam Pdut <noampdut@gmail.com>
2022-04-11 17:10:09 +0300

    pictures

--- chat/src/App.js
+++ chat/src/App.js
@@ -4,15 +4,16 @@ import Login from './Login/Login';
 import Register from './Register/Register';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import ChattingPage from './ChattingPage/ChattingPage';
+import MessagesBox from './ChattingPage/Contacts/MessagesBox';
+import AddContactButton from './ChattingPage/Contacts/AddContactButton';
 
 function App() {
   return (
     <BrowserRouter>
        <Routes>
-          <Route path='/' element={<Login/ >}></Route>
+          <Route path='/' element={<MessagesBox/ >}></Route>
           <Route path = '/Login' element={<Login/ >}></Route>
           <Route path = '/Register' element={<Register/ >}></Route>
-          <Route path = '/ChattingPage' element={<ChattingPage/ >}></Route>
        </Routes>
     </BrowserRouter>
   );
