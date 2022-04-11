2967185620e5a6c21eaa566de3dd1d53e456ccdd
lilach lacham <lilachlacham@gmail.com>
2022-04-11 17:46:43 +0300

    check

---
+++ chat/src/ChattingPage/Chatroom/ChatScreen/MassageGet.js
@@ -0,0 +1,15 @@
+import './massageGet.css';
+
+function MassageGet({text}) {
+
+    return (
+        <div className="media w-50 mb-3 myGet">
+          <div className="media-body ml-3">
+            <div className="bg-primary rounded py-2 px-3 mb-2">
+              <div className="text-small mb-0 text-white myGet">Test which is a njjjjjjkkkkkkkkkkkkkkkkkkkkkkkew approach all solutions</div>
+            </div>
+          </div>
+        </div>
+    );
+}
+export default MassageGet;
---
+++ chat/src/ChattingPage/Massages.js
@@ -0,0 +1,13 @@
+const Messages=[{
+    'text':"Hello!",
+    'get':true},
+    {'text':"Hii, how are you?",
+    'get':false},
+    {'text':"Great!",
+    'get':true}];
+
+function insertNewMessages(text){
+    Massages.push({text});
+}
+
+export {insertNewMessages};
